2f1bf77eece2cff1854dd0d66738dcd997f24ab7
DESKTOP-E1P73N6\kevin <kevin3571630@gmail.com>
2022-10-14 23:44:33 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// 靜態類別Bmw
+    /// </summary>
+    public static class Bmw
+    {
+        /// <summary>
+        /// Bmw車牌
+        /// </summary>
+        public static string name { get; set; }
+    }
+
+    /// <summary>
+    /// 非靜態類別 Benz
+    /// </summary>
+    public class Benz
+    {
+        /// <summary>
+        /// 名字
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 建構式
+        /// </summary>
+        public Benz()
+        {
+            name = "賓士外殼";
+        }
+        /// <summary>
+        /// 發動
+        /// </summary>
+        public void Start()
+        {
+            Console.WriteLine($"{name} 出發囉!!!!");
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ac = 55;
+            int a = 11111;
+            string b = "222";
+
+            int c = ac + Convert.ToInt32(b);
+            int e = ac + a;
+
+            string f = Aee(); // ctrl + 左鍵
+            Acc("aaa");
+
+            Console.WriteLine(Bmw.name);
+
+            // 實作化
+            Benz benz = new Benz();
+            Benz benz2 = new Benz();
+            benz2.name = "AMG";
+            benz2.Start();
+
+            Console.WriteLine(benz.name);
+            Console.WriteLine(benz2.name);
+
+
+
+            Console.WriteLine(c);
+            Console.WriteLine(e);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Acc Function
+        /// </summary>
+        /// <param name="a">a參數</param>
+        public static void Acc(string a)
+        {
+            Console.WriteLine($"ACC執行了 - {a}");
+            Console.WriteLine(string.Format("哈哈:{0}  {1}  {2}", 123, 456, 789));
+        }
+
+        public static string Aee()
+        {
+            Console.WriteLine("Aee執行了");
+            return "Aee";
+        }
+
+    }
+}
